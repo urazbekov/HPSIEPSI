d14153a89e35aa35925d1b5fcc2c4ffdb5522f7c
Bakytzhan <urazbekovb@mail.ru>
2019-11-20 12:51:22 +0100

    runnning with MINGW files added

--- HPsiEPsi/main.cpp
+++ HPsiEPsi/main.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <iostream>
-#include <gsl/gsl_errno.h>
+//#include <gsl/gsl_errno.h>
 #include <gsl/gsl_matrix.h>
-#include <gsl/gsl_odeiv2.h>
+//#include <gsl/gsl_odeiv2.h>
 #include <math.h>
 #include <vector>
 using namespace std;
@@ -37,7 +37,7 @@ generalParametersClass::generalParametersClass(int N_, double RMIN_, double RMAX
 class
 partitionClass {
 public:
-    partitionClass (bool energyLab, double energy, double m1, double m2, int z1, int z2);
+    partitionClass (bool energyLab, double energy, double m1, double m2, int z1, int z2, int N);
     double  hBarSquared =41.801651165221026;
     bool    energyLab;
     double  energy;
@@ -46,22 +46,25 @@ public:
     double  m, twoMuDevidedByhBarSquared;
     double  k, kSquared;
     double  n;
-    vector<double>  waveFunction;
+    vector<double>  waveFunctionRe;
+    vector<double>  waveFunctionIm;
  //   double  interactionKernel (double R);
 //    double  getAssimpFunctionAtV0 (double V0);
     bool    boundStateQ();
-    void    setWoodsSaxonParameters(double V0, double R0, double a0);
+    void    setWoodsSaxonParameters(double V0_r_, double R0_r, double a0_r_,
+      double V0_i_ , double R0_i_, double a0_i_ );
     double  kernelFunction(double R);
-
-private:
-    double  V0_WS, R0_WS, a0_WS;
+    double  V0_r, R0_r, a0_r;
+    double  V0_i, R0_i, a0_i;
     int l=0;
+private:
+
 };
 
 partitionClass::partitionClass
  (bool energyLabBool,  double energyDouble,
   double m1Double,     double m2Double,
-  int z1Int,           int z2Int){
+  int z1Int,           int z2Int, int N){
     energyLab   =energyLabBool;
     energy      =energyDouble;
     m1          =m1Double;
@@ -75,6 +78,9 @@ partitionClass::partitionClass
     kSquared                    = 2 *m *energy /hBarSquared;
     k                           = sqrt(abs(kSquared));
     n                           = z1 *z2 *1.43997 /hBarSquared /k;
+    waveFunctionRe.resize(N, 0.0);
+    waveFunctionIm.resize(N, 0.0);
+
 }
 
 
@@ -85,7 +91,7 @@ partitionClass::interactionPotential(char potentialChar, double R_){
     double var;
     switch(potentialChar){
          case "WS":
-            
+
      }
     return var;
 }
@@ -93,15 +99,19 @@ partitionClass::interactionPotential(char potentialChar, double R_){
 
 double
 partitionClass::kernelFunction(double R_){
-   return  kSquared +twoMuDevidedByhBarSquared *V0_WS /( 1 +exp( (R_ - R0_WS) /a0_WS))
-    - l *(l +1) /R_;
+   return  kSquared +twoMuDevidedByhBarSquared *V0_r /( 1 +exp( (R_ - R0_r) /a0_r))
+    - l *(l +1) /R_ /R_;
 }
 
 void
-partitionClass::setWoodsSaxonParameters(double V0_, double R0_, double a0_){
-    V0_WS  =V0_;
-    R0_WS  =R0_;
-    a0_WS  =a0_;
+partitionClass::setWoodsSaxonParameters(double V0_r_, double R0_r_, double a0_r_,
+double V0_i_ =0.0, double R0_i_ =1.250, double a0_i_ =0.650){
+    V0_r  =V0_r_;
+    R0_r  =R0_r_;
+    a0_r  =a0_r_;
+    V0_i  =V0_i_;
+    R0_i  =R0_i_;
+    a0_i  =a0_i_;
 }
 
 bool
@@ -112,31 +122,42 @@ partitionClass::boundStateQ(){
 
 void
 applyFiniteDiffMethodFor (partitionClass &partition, generalParametersClass &parameter){
-    partition.waveFunction.resize(parameter.N);
-    partition.waveFunction[0]=0.0;
-    partition.waveFunction[1]=0.1;
+    partition.waveFunctionRe[0]=0.0;
+    partition.waveFunctionRe[1]=0.1;
+    partition.waveFunctionIm[0]=0.0;
+    partition.waveFunctionIm[1]=0.1;
     for (int i=2; i < parameter.N; i++) {
-        partition.waveFunction[i] =
+        partition.waveFunctionRe[i] =
         (2 -parameter.H *parameter.H
         *partition.kernelFunction(parameter.R[i-1]))
-        *partition.waveFunction[i-1] -partition.waveFunction[i-2];
+        *partition.waveFunctionRe[i-1] -partition.waveFunctionRe[i-2]
+        -parameter.H *parameter.H *partition.twoMuDevidedByhBarSquared
+        *partition.V0_i /(1 +exp(parameter.R[i-1] - partition.R0_i) /partition.a0_i)
+        *partition.waveFunctionIm[i-1];
+
+        partition.waveFunctionIm[i] =
+        (2 -parameter.H *parameter.H
+        *partition.kernelFunction(parameter.R[i-1]))
+        *partition.waveFunctionIm[i-1] -partition.waveFunctionIm[i-2]
+        -parameter.H *parameter.H *partition.twoMuDevidedByhBarSquared
+        *partition.V0_i /(1 +exp(parameter.R[i-1] - partition.R0_i) /partition.a0_i)
+        *partition.waveFunctionRe[i-1];
     }
 }
 
 void
 applyNumerovMethodFor (partitionClass &partition, generalParametersClass &parameter){
     double gamma = parameter.H *parameter.H /12.0;
-    partition.waveFunction.resize(parameter.N);
-    partition.waveFunction[0]=0.0;
-    partition.waveFunction[1]=0.1;
+    partition.waveFunctionRe[0]=0.0;
+    partition.waveFunctionRe[1]=0.1;
     for (int i=2; i < parameter.N; i++) {
-        partition.waveFunction[i] =
+        partition.waveFunctionRe[i] =
         1. /(  1. +gamma *partition.kernelFunction( parameter.R[i] )  )
         *(
           (2. -10.0 *gamma *partition.kernelFunction(parameter.R[i-1]) )
-          *partition.waveFunction[i-1]
+          *partition.waveFunctionRe[i-1]
           - (1. +gamma *partition.kernelFunction(parameter.R[i-2]))
-          *partition.waveFunction[i-2]
+          *partition.waveFunctionRe[i-2]
           );
     }
 }
@@ -157,23 +178,23 @@ findRoot (double (*f)(double), double argument){
         rootCounter++;
         printf("%d \t %.9f \n",rootCounter, middleTest);
     }
-    
+
 }
 
 
 int
 main (void) {
     generalParametersClass generalParameters(201, 0.001, 40.001, 40, 1.E-6);
-    partitionClass firstPartition(false, -2.225, 1.0, 1.0, 0, 0);
-    firstPartition.setWoodsSaxonParameters(63.3693, 1.25, 0.65);
+    partitionClass firstPartition(false, -2.225, 1.0, 1.0, 0, 0, 201);
+    firstPartition.setWoodsSaxonParameters(63.368593, 1.25, 0.65, 30.0, 1.25, 0.65);
     applyNumerovMethodFor(firstPartition, generalParameters);
 
-    partitionClass secondPartition(false, -2.225, 1.0, 1.0, 0, 0);
-    secondPartition.setWoodsSaxonParameters(63.3693, 1.25, 0.65);
+    partitionClass secondPartition(false, -2.225, 1.0, 1.0, 0, 0, 201);
+    secondPartition.setWoodsSaxonParameters(63.368593, 1.25, 0.65);
     applyFiniteDiffMethodFor(secondPartition, generalParameters);
 
     for (int i=0; i<generalParameters.N; i++) {
-        printf("%.3F\t%.5f\t%.5f\n", generalParameters.R[i], firstPartition.waveFunction[i], secondPartition.waveFunction[i]);
+        printf("%.3f\t%.5f\t%.5f\n", generalParameters.R[i], firstPartition.waveFunctionIm[i], secondPartition.waveFunctionRe[i]);
     }
    // for (auto i = generalParameters.R.begin(); i != generalParameters.R.end(); ++i)
     //cout << *i << "\n";
