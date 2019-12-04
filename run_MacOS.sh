b7b676a1b4511ab4cfac929700491d0ebf95c6a1
Bakytzhan <zhan@MacBook-Pro-3.lan>
2019-12-04 09:34:47 +0100

    gsl ODE

--- run_MacOS.sh
+++ run_MacOS.sh
@@ -1,4 +1,6 @@
-g++ -Wall -I/usr/local/include -c main.cpp
-g++ -L/usr/local/lib -o main.exe main.o -lgsl -lgslcblas -lm
+uncrustify -c /usr/local/share/uncrustify/objc.cfg -f main.cpp -o main.cpp
+if g++ -Wall -I/usr/local/include -c main.cpp;
+g++ -L/usr/local/lib -o main.exe main.o -lgsl -lgslcblas -lm; then
 ./main.exe
 gnuplot gnuPlotScript
+fi
